4f1d78531b4c12d86cfd98dcfdd44cc50caabba7
hoyeonsong <hoyeon94614@gmail.com>
2020-10-06 14:49:27 +0900

    Create Customer Databases Table & Node.js Databases Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-          'id': 1,
-          'image': "https://placeimg.com/64/64/1",
-          'name': '오재욱',
-          'birthday': '940605',
-          'gender': '남자',
-          'job': 'NASA'
-        },
-        {
-          'id': 2,
-          'image': "https://placeimg.com/64/64/2",
-          'name': '송호연',
-          'birthday': '940614',
-          'gender': '여자',
-          'job': 'Google'
-        },
-      
-        {
-          'id': 3,
-          'image': "https://placeimg.com/64/64/3",
-          'name': '가나다라',
-          'birthday': '920324',
-          'gender': '남자',
-          'job': '회사원'
-        }
-      ]);
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send(rows);
+    }
+  );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
